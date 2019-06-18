a36c19d5f8d9f4d9d7a1bb8b3b5c31969350b62f
Inga <kreivyte.inga@gmail.com>
2019-06-18 19:09:56 +0300

    hero1

--- js/data.js
+++ js/data.js
@@ -1,1 +1,42 @@
 "use strict";
+
+var heroIcons = [
+    {
+        icon:'facebook',
+        link: '#'
+    },
+    {
+        icon: 'linkedin',
+        link: '#'
+    },
+    {
+        icon: 'dribbble',
+        link: '#'
+    },
+    {
+        icon: 'google-plus',
+        link:'#'
+    },
+    {
+        icon: 'twitter',
+        link: '#'
+    }
+];
+var footerIcons = [
+    {
+        icon:'facebook',
+        link: '#'
+    },
+    {
+        icon: 'twitter',
+        link: '#'
+    },
+    {
+        icon: 'linkedin',
+        link: '#'
+    },
+    {
+        icon: 'google-plus',
+        link:'#'
+    }
+]
--- js/functions.js
+++ js/functions.js
@@ -1,1 +1,9 @@
 "use strict";
+
+function generateIcons( data ) {
+    var HTML = '';
+    for ( var i=0; i<data.length; i++ ){
+        HTML+= '<a href="'+data[i].link+'" target="_blank" class="fa fa-'+data[i].icon+'"></a>';
+    }
+    return HTML;
+}
