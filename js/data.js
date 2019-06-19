d7c811f3eb49382d72d44a71647e2cb1f772c3bd
Inga <kreivyte.inga@gmail.com>
2019-06-19 17:47:12 +0300

    JSstart

--- js/data.js
+++ js/data.js
@@ -39,4 +39,51 @@ var footerIcons = [
         icon: 'google-plus',
         link:'#'
     }
+];
+
+var blogItems = [
+    {
+        photo: 'blog1.jpeg',
+        heading: 'There are many variations',
+        topic: 'Lifestyle',
+        block: [
+        {
+            date: '13 February 2018',
+            by: 'By Envato'
+        }
+        ],
+        description: 'Nulla in leo faucibus, porttitor augue sed, tristique sapien. Aliquam interdum semper euismod. In laoreet lobortis est, sed gravida ex semper et. Phasellus faucibus lorem at arcu congue, at congue justo rutrum. Fusce tempus varius arcu, eget semper diam lobortis eu. Cras sem turpis, consequat sit amet purus et, faucibus tincidunt est.',
+        link: '#'
+    },
+    {
+        photo: 'blog1.jpeg',
+        heading: 'Contrary to popular belief',
+        topic: 'Travel',
+        block: [
+        {
+            date: '13 February 2018',
+            by: 'By Envato'
+        }
+        ],
+        description:'Nulla in leo faucibus, porttitor augue sed, tristique sapien. Aliquam interdum semper euismod. In laoreet lobortis est, sed gravida ex semper et. Phasellus faucibus lorem at arcu congue, at congue justo rutrum. Fusce tempus varius arcu, eget semper diam lobortis eu. Cras sem turpis, consequat sit amet purus et, faucibus tincidunt est.',
+        link: '#'
+    },
+    {
+        photo: [
+            { first: 'blog3.1.jpg',
+            second: 'blog3.2.jpg',
+            third:  'blog3.3.jpg',
+            }
+        ],
+        heading: 'Lorem ipsum is not simple',
+        topic: 'Food &#38 Drinks',
+        block: [
+        {
+            date: '13 February 2018',
+            by: 'By Envato'
+        }
+        ],
+        description: 'Nulla in leo faucibus, porttitor augue sed, tristique sapien. Aliquam interdum semper euismod. In laoreet lobortis est, sed gravida ex semper et. Phasellus faucibus lorem at arcu congue, at congue justo rutrum. Fusce tempus varius arcu, eget semper diam lobortis eu. Cras sem turpis, consequat sit amet purus et, faucibus tincidunt est.',
+        link: '#'
+    },
 ]
--- js/functions.js
+++ js/functions.js
@@ -7,3 +7,19 @@ function generateIcons( data ) {
     }
     return HTML;
 }
+
+function generateBlog( data ){
+    var HTML = '';
+
+for ( var i=0; i< data.length; i++){
+    HTML=HTML+ `<div class="Blog">
+    <div class="img" style="background-image: url(../img/${data[i].photo});"></div>
+    <h3>${data[i].heading}</h3>
+    <h6>${data[i].topic}</h6>
+    <h6>${data[i].block}</h6>
+    <p>${data[i].description}...</p>
+    <a href="${data[i].link}" class="button">Read more</a>
+    </div>`;
+}
+    return HTML;
+}
