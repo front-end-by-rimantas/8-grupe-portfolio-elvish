288fbde454e5a7d95c62230991989460e91654af
IngaKreivyte <51168886+IngaKreivyte@users.noreply.github.com>
2019-06-25 17:25:19 +0300

    Update actions.js

--- js/actions.js
+++ js/actions.js
@@ -3,7 +3,6 @@
 
 document.getElementById('hero_icons').innerHTML = generateIcons( heroIcons );
 document.getElementById('footer_icons').innerHTML = generateIcons( footerIcons );
-document.getElementById('blog_list').innerHTML = generateBlog( blogItems );
 document.getElementById('blog_items').innerHTML = generateBlog ( blogItems );
 
 // services
